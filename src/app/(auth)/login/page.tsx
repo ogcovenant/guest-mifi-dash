ac51a084ccfd38bf463ab13f3a7c58835b0aeb3e
justcovenant <justcovenant@outlook.com>
2024-09-20 17:13:01 +0100

    feat: added responsiveness to the login screen

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,12 @@
 import Logo from "@/components/common/Logo";
-import React from "react";
 
 const Page = () => {
   return (
     <div className="flex w-full h-screen">
-      <div className="w-[60%] flex flex-col h-full items-center justify-center">
+      <div className="w-full md:w-[60%] flex flex-col h-full items-center justify-center p-8 lg:p-12 xl:p-0">
+        <div className="flex justify-center mb-4 md:hidden">
+          <Logo isDark={true} />
+        </div>
         <div className="text-center">
           <h1 className="font-bold text-2xl">Admin Login</h1>
           <p className="font-semibold">
@@ -12,9 +14,12 @@ const Page = () => {
           </p>
         </div>
         <div className="w-full mt-12 flex justify-center">
-          <form className="flex flex-col items-center gap-6 w-[50%]">
+          <form className="flex flex-col items-center gap-6 w-full xl:w-[50%]">
             <div className="flex flex-col gap-2 w-full">
-              <label htmlFor="email" className="font-semibold text-[#202224] opacity-80">
+              <label
+                htmlFor="email"
+                className="font-semibold text-[#202224] opacity-80"
+              >
                 Email address
               </label>
               <input
@@ -33,7 +38,9 @@ const Page = () => {
                 >
                   Password
                 </label>
-                <p className="text-[#202224] font-semibold opacity-60 cursor-pointer">Forgot Password?</p>
+                <p className="text-[#202224] font-semibold opacity-60 cursor-pointer">
+                  Forgot Password?
+                </p>
               </div>
               <input
                 type="password"
@@ -44,16 +51,26 @@ const Page = () => {
               />
             </div>
             <div className="flex items-center gap-1 w-full self-start">
-              <input type="checkbox" name="remember-password" id="remember-password"/>
+              <input
+                type="checkbox"
+                name="remember-password"
+                id="remember-password"
+              />
               <label htmlFor="remember-password">Remember Password</label>
             </div>
-            <button className="bg-[#442B88] w-full p-3 rounded-lg text-white font-bold">Sign In</button>
+            <button className="bg-[#442B88] w-full p-3 rounded-lg text-white font-bold">
+              Sign In
+            </button>
           </form>
         </div>
       </div>
-      <div className="bg-[#B2EBF2] w-[40%] flex flex-col items-center justify-center">
-        <Logo isDark={true}/>
-        <img src="/wifi-symbol.png" alt="Wifi illustration" className="w-[80%]"/>
+      <div className="bg-[#B2EBF2] w-[40%] md:flex flex-col items-center justify-center hidden">
+        <Logo isDark={true} />
+        <img
+          src="/wifi-symbol.png"
+          alt="Wifi illustration"
+          className="w-[80%]"
+        />
       </div>
     </div>
   );
