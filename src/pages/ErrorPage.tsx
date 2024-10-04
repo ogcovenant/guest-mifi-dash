9246705566db456b1ece0600b52bcf86f724f46c
justcovenant <justcovenant@outlook.com>
2024-10-04 02:29:37 +0100

    feat: added 404 page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,16 @@
+// import wifiSymbol from "@/assets/wifi-symbol-transparent.png"
+
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <div className="w-screen h-screen overflow-hidden flex flex-col justify-center items-center">
+      <div className="flex items-center justify-center">
+        <p className="text-7xl font-bold text-[#2B275D]">4</p>
+        {/* <img src={wifiSymbol} alt="Wifi Illustration" className="w-52"/> */}
+        <p className="text-7xl font-bold text-[#2B275D]">0</p>
+        <p className="text-7xl font-bold text-[#2B275D]">4</p>
+      </div>
+      <p>Ooops, looks like the page you are trying to access does not exists</p>
+    </div>
   )
 }
 
